d2042eb43d3b018c9075bb62227654ea67a3453d
Hanif Uddin <hanif.uddz@gmail.com>
2023-12-25 15:04:06 +0000

    Final Portfolio with some responsiveness, no animations

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+/* ----------------------- Scroll section active link ----------------------- */
 let sections = document.querySelectorAll('Section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -13,6 +14,10 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
-        }
+        };
     });
-}
+    /* ------------------------------ Sticky navbar ----------------------------- */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
