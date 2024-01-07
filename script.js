7f5607796c15423d03e8fda5211148eb5bf3dd5c
Hanif Uddin <hanif.uddz@gmail.com>
2024-01-07 18:14:24 +0000

    section functionality added

--- script.js
+++ script.js
@@ -2,22 +2,6 @@
 let sections = document.querySelectorAll('Section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        };
-    });
-};
-
 const icon = document.getElementById("dm-icon");
 
 icon.onclick = function darkMode() {
@@ -28,4 +12,65 @@ icon.onclick = function darkMode() {
     else {
         icon.firstChild.className = "bx bxs-moon";
     }
+ }
+
+ const homeButton = document.getElementById("home-bar");
+ const homeSection = document.getElementById("home");
+ const aboutButton = document.getElementById("about-bar");
+ const aboutSection = document.getElementById("about");
+ const portfolioButton = document.getElementById("portfolio-bar");
+ const portfolioSection = document.getElementById("portfolio");
+ const contactButton = document.getElementById("contact-bar");
+ const contactSection = document.getElementById("contact");
+
+ homeButton.onclick = function home() {
+    aboutSection.style.visibility = "hidden";
+    aboutSection.style.opacity = "0";
+    portfolioSection.style.visibility = "hidden";
+    portfolioSection.style.opacity = "0";
+    contactSection.style.visibility = "hidden";
+    contactSection.style.opacity = "0";
+    navLinks.forEach(links => {
+        links.classList.remove('active');
+        homeButton.classList.add('active');
+    })
+ }
+
+ aboutButton.onclick = function about() {
+    aboutSection.style.visibility = "visible";
+    aboutSection.style.opacity = "1";
+    portfolioSection.style.visibility = "hidden";
+    portfolioSection.style.opacity = "0";
+    contactSection.style.visibility = "hidden";
+    contactSection.style.opacity = "0";
+    navLinks.forEach(links => {
+        links.classList.remove('active');
+        aboutButton.classList.add('active');
+    })
+ }
+
+ portfolioButton.onclick = function portfolio() {
+    aboutSection.style.visibility = "hidden";
+    aboutSection.style.opacity = "0";
+    portfolioSection.style.visibility = "visible";
+    portfolioSection.style.opacity = "1";
+    contactSection.style.visibility = "hidden";
+    contactSection.style.opacity = "0";
+    navLinks.forEach(links => {
+        links.classList.remove('active');
+        portfolioButton.classList.add('active');
+    })
+ }
+
+ contactButton.onclick = function contact() {
+    portfolioSection.style.visibility = "hidden";
+    portfolioSection.style.opacity = "0";
+    aboutSection.style.visibility = "hidden";
+    aboutSection.style.opacity = "0";
+    contactSection.style.visibility = "visible";
+    contactSection.style.opacity = "1";
+    navLinks.forEach(links => {
+        links.classList.remove('active');
+        contactButton.classList.add('active');
+    })
  }
